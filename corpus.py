30e19758316d7f49282a2b520939ae051828131a
Alexander Efimov <alexandr.efimov@gmail.com>
2024-11-12 08:47:07 +0500

    Corpus parse_to_one

--- corpus.py
+++ corpus.py
@@ -2,10 +2,13 @@ from os import listdir
 from lemmatazer import Lemmatazer
 
 class Corpus:
-  def __init__(self, path = 'd/in/calgary/'):
+  path = 'd/in/calgary/'
+  file_out = 'd/corpus/out.txt'
+
+  def __init__(self, path = path, lm = Lemmatazer()):
     self.path = path
     self.files = (f for f in listdir(path) if f.endswith('.txt'))
-    self.lm = Lemmatazer()
+    self.lm = lm
   
   def parse(self, path_out = 'd/corpus/out/'):
     for f in self.files:
@@ -15,7 +18,17 @@ class Corpus:
       self.lm.stat(stop_len = False)
       print()
 
+  def parse_to_one(self, file = file_out):
+    words = []
+    for f in self.files:
+      self.lm.parse(self.path + f)
+      words += self.lm.words
+    self.lm.words = words
+    self.lm.save_text(file)
+    self.lm.stat(stop_len = False)
+
 
 if __name__ == "__main__":
   co = Corpus()
-  co.parse()
+  # co.parse()
+  co.parse_to_one()
